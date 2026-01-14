2bfc2a480505ca43adbb7af1d03c61c57d18e637
Marsel Nazirov <nazirov.mars96@gmail.com>
2026-01-14 15:24:42 +0500

    added search

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,16 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Application;
+use App\Models\Faculty;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $applications = Application::with(['user', 'faculty'])->latest()->paginate(5);
+        $faculties = Faculty::all();
+
+        $query = Application::with(['user', 'faculty']);
+
+        if ($request->filled('status')) {
+            $query->where('status', '=', $request->input('status'));
+        }
+
+        if ($request->filled('faculty_id')) {
+            $query->where('faculty_id', '=', $request->input('faculty_id'));
+        }
+
+        if ($request->filled('search')) {
+            $query->whereHas('user', function ($q) use ($request) {
+                $q->where('name', 'like', "%" . $request->input('search') . "%");
+            });
+        }
+
+        $applications = $query->latest()->paginate(10)->withQueryString();
 
         return view('admin.dashboard', [
-            'applications' => $applications
+            'faculties' => $faculties,
+            'applications' => $applications,
         ]);
     }
 
